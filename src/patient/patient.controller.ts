fcef736029bbc710172f6623da4ab7c8dce0de6c
rapatte <agayoso@wemanity.com>
2022-03-14 15:02:14 +0100

    fix: controller path

--- src/patient/patient.controller.ts
+++ src/patient/patient.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import { Patient } from './patient.entity';
 import { PatientService } from './patient.service';
 
-@Controller()
+@Controller('/patient')
 export class PatientController {
   constructor(private readonly patientService: PatientService) {}
 
-  @Get('/patient')
+  @Get()
   getAllPatient(): Promise<Patient[]> {
     return this.patientService.getAllPatient();
   }
--- src/practitioner/practitioner.controller.ts
+++ src/practitioner/practitioner.controller.ts
@@ -2,11 +2,11 @@ import { Controller, Get } from '@nestjs/common';
 import { Practitioner } from './practitioner.entity';
 import { PractitionerService } from './practitioner.service';
 
-@Controller()
+@Controller('/practitioner')
 export class PractitionerController {
   constructor(private readonly practitionerService: PractitionerService) {}
 
-  @Get('/practitioner')
+  @Get()
   getAllPractitioner(): Promise<Practitioner[]> {
     return this.practitionerService.getAllPractitioner();
   }
