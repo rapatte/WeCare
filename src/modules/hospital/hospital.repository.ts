d47f6e82334558d0be91c1d030f13df3b1d45f3a
filmons <filmonseare@gmail.com>
2022-04-11 17:15:30 +0200

    get hopital by id

--- src/modules/hospital/hospital.repository.ts
+++ src/modules/hospital/hospital.repository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, EntityManager } from 'typeorm';
+import { EntityRepository, EntityManager, Any } from 'typeorm';
 import { Hospital } from './hospital.entity';
 
 @EntityRepository()
@@ -8,8 +8,17 @@ class HospitalRepository {
   async getAllHospital() {
     return await this.manager.find(Hospital);
   }
-
+  async getOneHospitalById(id: number) {
+    return await this.manager.findOne(Hospital, id);
+  }
+  async DeliteOne(id: number) {
+    return await this.manager.delete(Hospital, id);
+  }
   async addAHospital(hospital: Hospital) {
+<<<<<<< HEAD:src/modules/hospital/hospital.repository.ts
+=======
+    console.log(hospital);
+>>>>>>> 3e276da ( get hopital by id):src/hospital/hospital.repository.ts
     return await this.manager.save(Hospital, hospital);
   }
 }
--- src/modules/hospital/hospital.service.ts
+++ src/modules/hospital/hospital.service.ts
@@ -1,11 +1,31 @@
+<<<<<<< HEAD:src/modules/hospital/hospital.service.ts
 import { Inject, Injectable } from '@nestjs/common';
 import { ApiError } from 'src/helpers/error';
 import {
   IHospitalRepository,
   IHospitalService,
 } from 'src/helpers/interfaces/hospital.interface';
+=======
+import {
+  HttpException,
+  HttpStatus,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
+>>>>>>> 3e276da ( get hopital by id):src/hospital/hospital.service.ts
 import { Hospital } from './hospital.entity';
 import HospitalRepository from './hospital.repository';
+<<<<<<< HEAD:src/modules/hospital/hospital.service.ts
+=======
+import { identity } from 'rxjs';
+
+export interface IHospitalRepository {
+  getAllHospital(): Promise<Hospital[]>;
+  getOneHospitalById(id: number): Promise<Hospital>;
+  addAHospital(hospital: Hospital): Promise<Hospital>;
+}
+>>>>>>> 3e276da ( get hopital by id):src/hospital/hospital.service.ts
 @Injectable()
 export class HospitalService implements IHospitalService {
   constructor(
@@ -15,6 +35,23 @@ export class HospitalService implements IHospitalService {
 
   async getAllHospital(): Promise<Hospital[]> {
     return this.hospitalRepository.getAllHospital();
+  }
+
+  async getOneHospitalById(id: number): Promise<Hospital> {
+    const hospital = await this.hospitalRepository.getOneHospitalById(id);
+    if (!hospital) {
+      throw new Error('hospital not found.');
+    }
+    // console.log(hospital, 'bbbbb');
+    return hospital;
+  }
+  async getOneHospitalById(id: number): Promise<Hospital> {
+    const hospital = await this.hospitalRepository.getOneHospitalById(id);
+    if (!hospital) {
+      throw new Error('hospital not found.');
+    }
+    // console.log(hospital, 'bbbbb');
+    return hospital;
   }
 
   async addAHospital(hospital): Promise<Hospital> {
